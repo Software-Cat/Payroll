84b90860a26fe593d337f784e5db721eb594fe27
Software-Cat <51037053+Software-Cat@users.noreply.github.com>
2022-07-01 23:25:51 +1000

    Implement employee class

---
+++ src/main/java/io/github/softwarecat/payroll/Employee.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2022 Bowen Wu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.softwarecat.payroll;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    @Getter
+    @Setter
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private String role;
+
+    private Employee() {
+    }
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) && Objects.equals(name, employee.name) && Objects.equals(role, employee.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, role);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Employee.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("role='" + role + "'")
+                .toString();
+    }
+}
