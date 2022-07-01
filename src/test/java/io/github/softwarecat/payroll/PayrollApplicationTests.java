990840ba73664e2e2a46a8d208a3970df3746577
Software-Cat <51037053+Software-Cat@users.noreply.github.com>
2022-07-01 22:59:38 +1000

    Initial commit

---
+++ src/test/java/io/github/softwarecat/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.softwarecat.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
